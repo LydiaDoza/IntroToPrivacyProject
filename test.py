9fcf7c0bf60b00f7a3c3b4784a7ae13eb8e9c079
Timothy hall <59980086+halltim91@users.noreply.github.com>
2024-03-09 23:22:18 -0800

    finished random_action

--- test.py
+++ test.py
@@ -1,55 +1,60 @@
 import init as db
 import random
+from faker import Faker
 
-def random_action(engine):
-    #operation = random.choices(list(db.Operation), weights = [0, 1, 3, 2])
-    operation = random.choices(list(db.Operation), weights = [0, 0, 1, 0])[0]
-    print(operation)
+def random_action(engine, can_delete=True):
+    operation = random.choices(list(db.Operation), weights = [0, .1, .5, .4])[0]
     entity = db.select_random_employee(engine)
     data = db.get_random_account(engine)
 
     if operation == db.Operation.update:
-        column= random.choice(list(db.data_schema.keys())[1:])
+        column= random.choice(list(db.data_schema.keys())[1:-1])
         if column == 'annual_income':
-            pass
+            new_value = random.randint(10000, 10000000)
         elif column == 'applicant_age':
-            pass
+            new_value = random.randint(21, 79)
         elif column == 'work_experience':
-            pass
+            new_value = random.randint(0, 20)
         elif column == 'marital_status':
-            #new_value= 'married' if data[]
-            pass
+            new_value= 'married' if data[5] == 'single' else 'single'
         elif column == 'house_ownership':
-            #new_value= 
-            pass
+            new_value= 'owned' if data[6] == 'rented' else 'rented' 
         elif column == 'vehicle_ownership':
-            pass
+            new_value= 'yes' if data[7] == 'no' else 'no'
         elif column == 'occupation':
-            pass
+            fake = Faker()
+            new_value = fake.job()
+            if len(new_value) > 40:
+                x = new_value.split(',')
+                if len(x) > 1:
+                    new_value = x[0]
+                new_value.replace("'", "\\'")
+                new_value = new_value[:40]
         elif column == 'residence_city':
-            pass
+            fake = Faker()
+            new_value = fake.city()
         elif column == 'residence_state':
-            pass
+            fake = Faker()
+            new_value = fake.state()
         elif column == 'years_in_current_employment':
-            pass
+            new_value = random.randint(0, 15)
         elif column == 'years_in_current_residence':
-            pass
+            new_value = random.randint(0, 15)
         elif column == 'loan_default_risk':
-            pass
+            new_value = True if data[13] == False else False
         else:
-            print("Some how you got an invalid column name to generate")
+            print(f"Some how you got an invalid column name to generate {column}")
             return
+        db.update_data(data[1], column, new_value, engine, index=data[0])
+
     elif operation == db.Operation.view:
         purpose = random.choice([db.Purpose.audit, db.Purpose.review])
         role = random.choice(list(db.Role))
         policy = db.add_access_policy(role, purpose, engine)
         db.log_view(policy, entity, data[0], engine)
-        pass
+
     elif operation == db.Operation.delete:
-        pass
-    
-    #purpose
-    # data id
-    # operation
-    # new value
-    # column name
+        if can_delete == True:
+            db.soft_delete(data[0], engine)
+        else:
+            random_action(engine, can_delete=False)
